ed244e7f9c50854b2ddf1edf466e0e10304c88a7
bwnstk <info@flimmerkiste.xyz>
2020-11-20 23:04:37 +0100

    :fire: Remove unnecesarry code and some file changes

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,7 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+function sum(a, b) {
+  return a + b;
+}
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("adds 1 + 2 to equal 3", () => {
+  expect(sum(1, 2)).toBe(3);
 });
--- client/src/stories/Button.js
+++ client/src/stories/Button.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './button.css';
+import React from "react";
+import PropTypes from "prop-types";
 
 /**
  * Primary UI component for user interaction
  */
 export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  const mode = primary ? "button--primary" : "button--secondary";
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      className={["button", `button--${size}`, mode].join(" ")}
       style={backgroundColor && { backgroundColor }}
       {...props}
     >
@@ -31,7 +30,7 @@ Button.propTypes = {
   /**
    * How large should the button be?
    */
-  size: PropTypes.oneOf(['small', 'medium', 'large']),
+  size: PropTypes.oneOf(["small", "medium", "large"]),
   /**
    * Button contents
    */
@@ -45,6 +44,6 @@ Button.propTypes = {
 Button.defaultProps = {
   backgroundColor: null,
   primary: false,
-  size: 'medium',
+  size: "medium",
   onClick: undefined,
 };
