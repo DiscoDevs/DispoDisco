d5bc1306417058553d4b4fa920929a1365d9673a
bwnstk <info@flimmerkiste.xyz>
2020-11-21 01:59:45 +0100

    :building_construction: Reconfigure storybook

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -1,4 +1,5 @@
+import "!style-loader!css-loader!./main.css";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-}
+};
--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,40 @@
 import React from "react";
+import { createGlobalStyle } from "styled-components";
 
 import { Header } from "./stories/Header";
+import { Badge } from "./stories/Badge";
+
+const GlobalStyle = createGlobalStyle`
+  :root{
+    /* CSS HSL */
+--green-crayola: hsla(158, 68%, 42%, 1);
+--primary-color: hsla(158, 68%, 42%, 1);
+--eerie-black: hsla(0, 0%, 14%, 1);
+--cultured: hsla(0, 0%, 94%, 1);
+--orange-crayola: hsla(17, 98%, 59%, 1);
+--shocking-pink: hsla(312, 82%, 55%, 1);
+--cyber-yellow: hsla(50, 99%, 50%, 1);
+--jonquil: hsla(48, 82%, 55%, 1);
+--carnelian: hsla(0, 75%, 40%, 1);
+--dark-red: hsla(0, 86%, 29%, 1);
+--electric-indigo: hsla(259, 82%, 55%, 1);
+  }
+  #root {
+    margin: 0;
+    padding: 0;
+    font-family: Open-Sans, Helvetica, Sans-Serif;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <>
+      <GlobalStyle />
+      <div className="App">
+        <Header />
+        <Badge primary></Badge>
+      </div>
+    </>
   );
 }
 
