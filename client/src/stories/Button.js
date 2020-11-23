f07a6201c246074db6d845aeacbf80ec3f55f0a4
bwnstk <info@flimmerkiste.xyz>
2020-11-23 19:50:02 +0100

    :bento: Add PlusButton

--- client/src/stories/Button.js
+++ client/src/stories/Button.js
@@ -1,50 +1,32 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './button.css';
-
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components/macro";
+import ButtonPlus from "./assets/buttonPlus.svg";
 /**
  * Primary UI component for user interaction
  */
-export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+
+const ButtonElement = styled.div`
+  border-radius: 50%;
+`;
+
+const ButtonImage = styled.img``;
+
+export const Button = ({ label }) => {
   return (
-    <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={backgroundColor && { backgroundColor }}
-      {...props}
-    >
+    <ButtonElement>
+      <ButtonImage src={ButtonPlus} />
       {label}
-    </button>
+    </ButtonElement>
   );
 };
 
 Button.propTypes = {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary: PropTypes.bool,
-  /**
-   * What background color to use
-   */
-  backgroundColor: PropTypes.string,
-  /**
-   * How large should the button be?
-   */
-  size: PropTypes.oneOf(['small', 'medium', 'large']),
-  /**
-   * Button contents
-   */
-  label: PropTypes.string.isRequired,
-  /**
-   * Optional click handler
-   */
+  // type: PropTypes.oneOf(["round"]),
+
+  label: PropTypes.string,
+
   onClick: PropTypes.func,
 };
 
-Button.defaultProps = {
-  backgroundColor: null,
-  primary: false,
-  size: 'medium',
-  onClick: undefined,
-};
+Button.defaultProps = {};
