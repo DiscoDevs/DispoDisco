fcb36086fe8b3fd4936ce9625ed5e80fc972103a
domko95 <dominique.kolbe1995@googlemail.com>
2020-11-24 12:31:59 +0100

    :lipstick: Style input field

--- client/src/stories/Input.js
+++ client/src/stories/Input.js
@@ -1,5 +1,17 @@
 import React from "react";
+import styled from "styled-components/macro";
 
 export function Input() {
-  return <input type="text" placeholder="placeholder" />;
+  const Component = styled.input`
+    padding: 0.5rem 1rem;
+    ::placeholder {
+      font-style: italic;
+    }
+  `;
+
+  return (
+    <form>
+      <Component type="text" placeholder="placeholder" />
+    </form>
+  );
 }
