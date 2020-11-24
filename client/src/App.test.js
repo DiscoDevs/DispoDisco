915268fda91e27c793aee5fd1c0c3cfdc3b504a6
Benjamin Weinstock <37471898+bwnstck@users.noreply.github.com>
2020-11-24 12:06:00 +0100

    🗑 Removed boilerplate files (#27)
    * :wastebasket: Removed boilerplate files
    
    * :fire: Remove css

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,8 +2,6 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders App", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
 });
