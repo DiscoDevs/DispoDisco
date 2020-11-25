298a98ad8cd04a329710d042e5380d929f96cf75
bwnstk <info@flimmerkiste.xyz>
2020-11-25 17:25:10 +0100

    :sparkles: Add miniHeader
    fixes #46

--- client/src/stories/Header.stories.js
+++ client/src/stories/Header.stories.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import { HeaderMain } from "../components/HeaderMain";
 import { HeaderMenu } from "../components/HeaderMenu";
+import { HeaderMini } from "../components/HeaderMini";
 
 export default {
   title: "DispoDisco/Header",
@@ -10,7 +11,9 @@ export default {
 
 const MenuTemplate = (args) => <HeaderMenu {...args} />;
 const MainTemplate = (args) => <HeaderMain {...args} />;
+const MiniTemplate = (args) => <HeaderMini {...args} />;
 
 export const HeaderMenus = MenuTemplate.bind({});
 
 export const HeaderMains = MainTemplate.bind({});
+export const HeaderMinimized = MiniTemplate.bind({});
