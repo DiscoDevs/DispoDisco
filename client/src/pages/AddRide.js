19a95055ce9e8ce95bdd4274fb7901f8a70dd44e
bwnstk <info@flimmerkiste.xyz>
2020-11-30 19:38:53 +0100

    :construction: Start to Add Info and Checkbox to Rides

--- client/src/pages/AddRide.js
+++ client/src/pages/AddRide.js
@@ -10,15 +10,12 @@ import Input from "../components/Input";
 import Button from "../components/Button";
 
 const PageWrapper = styled.div`
-  position: fixed;
-  height: 100%;
+  min-height: 100%;
   width: 100%;
-  background: var(--text-secondary);
-  overflow: auto;
-`;
-
-const ContentWrapper = styled.div`
+  margin: auto;
   padding-top: 12rem;
+
+  background: var(--text-secondary);
 `;
 
 const Form = styled.form`
@@ -26,7 +23,8 @@ const Form = styled.form`
   flex-direction: column;
   align-items: center;
   margin: 2rem auto;
-  width: 80vw;
+  width: 80%;
+  max-width: 400px;
   > * {
     margin-top: 0.7rem;
   }
@@ -46,6 +44,17 @@ const Form = styled.form`
     }
   }
 `;
+const StyledDetails = styled.details`
+  color: var(--text-primary);
+  text-align: left;
+  margin: 1rem 0;
+  width: 100%;
+  textarea {
+    width: 100%;
+  }
+`;
+
+const OutputContainer = styled.div``;
 
 export default function AddRide() {
   const [title, setTitle] = useState("");
@@ -55,76 +64,92 @@ export default function AddRide() {
   const [rider, setRider] = useState("");
   const [priority, setPriority] = useState("normal");
   const [cargo, setCargo] = useState("s");
+  const [info, setInfo] = useState("");
+  const [checkBoxes, setCheckboxes] = useState("");
   const history = useHistory();
 
   return (
     <PageWrapper>
       <HeaderMain />
-      <ContentWrapper>
-        <Card
-          type="dayRide"
-          labels={
-            <>
-              <Badge type="cargo" />
-              <Badge type="direct" />
-              <Badge type="carriage" />
-            </>
-          }
+      <Card
+        type="dayRide"
+        labels={
+          <>
+            <Badge type="cargo" />
+            <Badge type="direct" />
+            <Badge type="carriage" />
+          </>
+        }
+      />
+      <Form
+        onSubmit={(event) => {
+          event.preventDefault();
+          addRide({
+            title: title,
+            start: start,
+            dest: dest,
+            date: date,
+            assignment: rider,
+            priority: priority,
+            cargo: cargo,
+          });
+          history.push("/rides");
+        }}
+      >
+        <Input
+          type="text"
+          placeholder="Titel"
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
         />
-        <Form
-          onSubmit={(event) => {
-            event.preventDefault();
-            addRide({
-              title: title,
-              start: start,
-              dest: dest,
-              date: date,
-              assignment: rider,
-              priority: priority,
-              cargo: cargo,
-            });
-            history.push("/rides");
-          }}
-        >
-          <Input
-            type="text"
-            placeholder="Titel"
-            value={title}
-            onChange={(event) => setTitle(event.target.value)}
-          />
-          <Input
-            type="text"
-            placeholder="Start"
-            value={start}
-            onChange={(event) => setStart(event.target.value)}
-          />
-          <Input
-            type="text"
-            placeholder="Ziel"
-            value={dest}
-            onChange={(event) => setDest(event.target.value)}
-          />
-          <Input
-            type="datetime-local"
-            value={date}
-            onChange={(event) => setDate(event.target.value)}
-          />
-          <Input
-            type="text"
-            placeholder="Fahrer"
-            value={rider}
-            onChange={(event) => setRider(event.target.value)}
-          />
-          <div>
-            <Badge type="direct" onClick={() => setPriority("direct")} />
-            <Badge type="onTime" onClick={() => setPriority("onTime")} />
-            <Badge type="carriage" onClick={() => setCargo("carriage")} />
-            <Badge type="cargo" label="m" onClick={() => setCargo("m")} />
-            <Badge type="cargo" label="l" onClick={() => setCargo("l")} />
-          </div>
-          <Button type="submit" design="addRide" label="Fahrt hinzufügen" />
-        </Form>
-      </ContentWrapper>
+        <Input
+          type="text"
+          placeholder="Start"
+          value={start}
+          onChange={(event) => setStart(event.target.value)}
+        />
+        <Input
+          type="text"
+          placeholder="Ziel"
+          value={dest}
+          onChange={(event) => setDest(event.target.value)}
+        />
+        <Input
+          type="datetime-local"
+          value={date}
+          onChange={(event) => setDate(event.target.value)}
+        />
+        <Input
+          type="text"
+          placeholder="Fahrer"
+          value={rider}
+          onChange={(event) => setRider(event.target.value)}
+        />
+        <StyledDetails>
+          <summary>Info & Todo-Liste</summary>
+          <h3>Infos</h3>
+          <textarea name="InfoInput" cols="30" rows="10" />
+          <h3>Todo-Liste</h3>
+          <OutputContainer>
+            {checkBoxes && (
+              <ul>
+                {checkBoxes.map((entry, index) => (
+                  <li key={index}>{entry} </li>
+                ))}
+              </ul>
+            )}
+          </OutputContainer>
+          <input type="text" />
+        </StyledDetails>
+        <div>
+          <Badge type="direct" onClick={() => setPriority("direct")} />
+          <Badge type="onTime" onClick={() => setPriority("onTime")} />
+          <Badge type="carriage" onClick={() => setCargo("carriage")} />
+          <Badge type="cargo" label="m" onClick={() => setCargo("m")} />
+          <Badge type="cargo" label="l" onClick={() => setCargo("l")} />
+        </div>
+        <Button type="submit" design="addRide" label="Fahrt hinzufügen" />
+      </Form>
     </PageWrapper>
   );
 }
