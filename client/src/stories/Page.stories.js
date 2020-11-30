f0820c599e12bf11855ceee84f8121bfc15a89bb
Benjamin Weinstock <37471898+bwnstck@users.noreply.github.com>
2020-11-30 19:43:23 +0100

    ✨ Add Tour info (#93)
    * :bug: Fix Badges not showing up on Cards
    
    * :sparkles: Add RideInfo and disabled InfoButton on Card when on RideInfo
    
    * :sparkles: Add Todo-Component for Ride Info
    
    * :sparkles: Add Todo to Pages and make color white
    
    * :art: Removed comments and add macro to styled

--- client/src/stories/Page.stories.js
+++ client/src/stories/Page.stories.js
@@ -3,6 +3,7 @@ import MainMenu from "../pages/MainMenu";
 import Rides from "../pages/Rides";
 import Launch from "../pages/Launch";
 import AddRide from "../pages/AddRide";
+import RideInfo from "../pages/RideInfo";
 
 export default {
   title: "DispoDisco/Page",
@@ -13,8 +14,10 @@ const LaunchTemplate = (args) => <Launch {...args} />;
 const MainTemplate = (args) => <MainMenu {...args} />;
 const RidesTemplate = (args) => <Rides {...args} />;
 const AddRideTemplate = (args) => <AddRide {...args} />;
+const RideInfoTemplate = (args) => <RideInfo {...args} />;
 
 export const LaunchPage = LaunchTemplate.bind({});
 export const MainMenuPage = MainTemplate.bind({});
 export const RidesPage = RidesTemplate.bind({});
 export const AddRidePage = AddRideTemplate.bind({});
+export const RideInfoPage = RideInfoTemplate.bind({});
