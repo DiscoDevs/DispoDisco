4215f0ee7735cfd2fdff40eeed448e0f95347b97
bwnstk <info@flimmerkiste.xyz>
2020-11-30 16:30:32 +0100

    :sparkles: Add Todo to Pages and make color white

--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import PropTypes from "prop-types";
 
 const TodoElement = styled.div`
+  color: var(--text-primary);
   display: flex;
   align-items: center;
   justify-content: center;
