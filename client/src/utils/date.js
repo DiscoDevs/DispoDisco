4a0fac00ff19101f014365adacb9abc62f8cb1f6
domko95 <dominique.kolbe1995@googlemail.com>
2020-12-01 10:32:40 +0100

    :bug: Fix currentDate format for every combination

--- client/src/utils/date.js
+++ client/src/utils/date.js
@@ -1,10 +1,18 @@
 export function getCurrentDate() {
   const currentDate = new Date();
   const today =
-    currentDate.getDate() < 10
+    currentDate.getDate() < 10 && currentDate.getMonth() < 10
+      ? `${currentDate.getFullYear()}-0${
+          currentDate.getMonth() + 1
+        }-0${currentDate.getDate()}`
+      : currentDate.getDate() < 10
       ? `${currentDate.getFullYear()}-${
           currentDate.getMonth() + 1
         }-0${currentDate.getDate()}`
+      : currentDate.getMonth() < 10
+      ? `${currentDate.getFullYear()}-0${
+          currentDate.getMonth() + 1
+        }-${currentDate.getDate()}`
       : `${currentDate.getFullYear()}-${
           currentDate.getMonth() + 1
         }-${currentDate.getDate()}`;
