e8c1c8d415c6daaf8c0ad64bf26cd955a27c3044
domko95 <dominique.kolbe1995@googlemail.com>
2020-12-01 10:37:22 +0100

    :bulb: Add comment for understandability in date.js

--- client/src/utils/date.js
+++ client/src/utils/date.js
@@ -1,5 +1,7 @@
 export function getCurrentDate() {
   const currentDate = new Date();
+  // Following code is super hacky, gonna be changed, just needed it to work for now.
+  // Need fixed format for currentDate to be able to read data from database.
   const today =
     currentDate.getDate() < 10 && currentDate.getMonth() < 10
       ? `${currentDate.getFullYear()}-0${
