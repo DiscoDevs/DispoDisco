15689b93005dcfc53aed9b7cea0fe728e39e0430
domko95 <dominique.kolbe1995@googlemail.com>
2020-12-08 11:08:17 +0100

    :sparkles: Add riders page

---
+++ client/src/pages/Rider.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+import CardRider from "../components/CardRider";
+import { getSortedData } from "../utils/api";
+import Header from "../components/Header";
+import styled from "styled-components";
+import ToursGrid from "../components/helpers/ToursGrid";
+
+const PageWrapper = styled.div`
+  position: fixed;
+  overflow: auto;
+  height: 100%;
+  width: 100%;
+
+  background: var(--gradient-dark);
+  & > *:not(:first-child) {
+    margin: 1rem auto;
+  }
+  & > :nth-child(2) {
+    margin-top: clamp(9rem, 25vw, 200px);
+  }
+`;
+
+const Rider = () => {
+  const [riders, setRiders] = useState([]);
+
+  useEffect(() => {
+    const doFetch = async () => {
+      const data = await getSortedData({
+        collectionName: "riders",
+        dataName: "alias",
+      });
+      console.log({ data });
+      setRiders(data);
+    };
+    doFetch();
+  }, []);
+
+  console.log({ riders });
+  return (
+    <PageWrapper>
+      <Header title="Fahrer hinzufügen" />
+      <ToursGrid>
+        {riders?.map((rider) => (
+          <CardRider
+            key={rider._id}
+            name={rider.name}
+            alias={rider.alias}
+            dateOfBirth={rider.dateOfBirth}
+            phone={rider.phone}
+            picture={rider.picture}
+            color={rider.color}
+          />
+        ))}
+      </ToursGrid>
+    </PageWrapper>
+  );
+};
+export default Rider;
--- client/src/stories/Page.stories.js
+++ client/src/stories/Page.stories.js
@@ -5,6 +5,7 @@ import ToursToday from "../pages/ToursToday";
 import Launch from "../pages/Launch";
 import AddTour from "../pages/AddTour";
 import TourInfo from "../pages/TourInfo";
+import Rider from "../pages/Rider";
 
 export default {
   title: "DispoDisco/Page",
@@ -17,6 +18,7 @@ const ToursTemplate = (args) => <Tours {...args} />;
 const ToursTodayTemplate = (args) => <ToursToday {...args} />;
 const AddRideTemplate = (args) => <AddTour {...args} />;
 const RideInfoTemplate = (args) => <TourInfo {...args} />;
+const RiderTemplate = (args) => <Rider {...args} />;
 
 export const LaunchPage = LaunchTemplate.bind({});
 export const MainMenuPage = MainTemplate.bind({});
@@ -24,3 +26,4 @@ export const ToursPage = ToursTemplate.bind({});
 export const ToursTodayPage = ToursTodayTemplate.bind({});
 export const AddRidePage = AddRideTemplate.bind({});
 export const RideInfoPage = RideInfoTemplate.bind({});
+export const RiderPage = RiderTemplate.bind({});
