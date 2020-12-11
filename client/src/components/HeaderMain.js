08284c19c787691f0b4ca0b8c2c415ff66986557
domko95 <dominique.kolbe1995@googlemail.com>
2020-12-11 11:13:31 +0100

    :sparkles: Add datepicker on toggle

--- client/src/components/HeaderMain.js
+++ client/src/components/HeaderMain.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components/macro";
 import CalendarIcon from "../assets/calendar.svg";
 import FilterIcon from "../assets/filter.svg";
@@ -15,14 +15,32 @@ const IconContainer = styled.div`
   }
 `;
 
-const HeaderMain = () => (
-  <Header>
-    <Infobox>Next Stop 1:30h</Infobox>
-    <IconContainer>
-      <img src={CalendarIcon} alt="Calendar" />
-      <img src={FilterIcon} alt="Filter" />
-    </IconContainer>
-  </Header>
-);
+const HeaderMain = () => {
+  const [date, setDate] = useState(false);
+  const [datePicker, setDatePicker] = useState(<></>);
+
+  useEffect(() => {
+    if (date) {
+      setDatePicker(<input type="date" />);
+    } else {
+      setDatePicker(<></>);
+    }
+  }, [date]);
+
+  return (
+    <Header>
+      <Infobox>Next Stop 1:30h</Infobox>
+      <IconContainer>
+        {datePicker}
+        <img
+          src={CalendarIcon}
+          alt="Calendar"
+          onClick={() => (date === false ? setDate(true) : setDate(false))}
+        />
+        <img src={FilterIcon} alt="Filter" />
+      </IconContainer>
+    </Header>
+  );
+};
 
 export default HeaderMain;
