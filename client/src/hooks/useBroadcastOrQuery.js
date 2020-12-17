6955169486e525cf07d0afbfb8d3e423eea1e6dc
bwnstk <info@flimmerkiste.xyz>
2020-12-17 17:33:45 +0100

    :art: Add PWA

---
+++ client/src/hooks/useBroadcastOrQuery.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
+import { getSortedDataByQuery } from "../utils/api";
+import useBroadcastUpdate from "./useBroadcastUpdate";
+
+const useBroadcastOrQuery = ({ endpoint, today, option = "today" }) => {
+  const broadcastedTours = useBroadcastUpdate(`/api/${endpoint}`);
+
+  const { isLoading, isError, data, error, refetch } = useQuery(
+    [endpoint, option],
+    () =>
+      getSortedDataByQuery({
+        collectionName: endpoint,
+        type: "date",
+        query: today,
+      })
+  );
+  console.log({ data }, { broadcastedTours });
+  if (data?.length <= broadcastedTours?.length) {
+    return data;
+  }
+  return data;
+};
+
+export default useBroadcastOrQuery;
